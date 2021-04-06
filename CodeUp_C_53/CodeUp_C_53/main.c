428d34d6a31a97172dc7d82689fa384b6f20fa64
Eroon Sung <sung7290@gmail.com>
2021-04-06 11:46:04 +0900

    20210406

---
+++ CodeUp_C_53/CodeUp_C_53/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  CodeUp_C_53
+//
+//  Created by Leehyun Sung on 06/04/2021.
+//  Copyright © 2021 Eroon. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    int a;
+    scanf("%d", &a);
+    printf("%d", !a);
+    return 0;
+}
+
---
+++ CodeUp_C_59/CodeUp_C_59/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  CodeUp_C_59
+//
+//  Created by Leehyun Sung on 06/04/2021.
+//  Copyright © 2021 Eroon. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    int a;
+    scanf("%d", &a);
+    printf("%d", ~a);
+    return 0;
+}
